4b499a1fc718d5c4ac9aed5974a9e2d8497ffe40
EyK-26 <efeyagizkiziltas@gmail.com>
2023-12-08 12:10:12 +0100

    correct style2

--- resources/js/components/Messages.jsx
+++ resources/js/components/Messages.jsx
@@ -1,16 +1,18 @@
 import React, { useContext } from "react";
 import UserContext from "../myApp/context/UserContext";
+import { Link, useNavigate } from "react-router-dom";
 
 const Messages = () => {
     const { state } = useContext(UserContext);
 
     return (
         <>
-            {state.messages && (
+            {state.messages.length > 0 && (
                 <ul className="messages">
                     {state.messages.map((el, idx) => (
                         <li key={idx}>{el}</li>
                     ))}
+                    <Link to="/">back to homepage</Link>
                 </ul>
             )}
         </>
