306db4f30e10d4dfc5e4357561888a847f0dfbaa
EyK-26 <efeyagizkiziltas@gmail.com>
2023-12-18 17:12:43 +0100

    42

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Offer;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -12,30 +13,35 @@ class OfferController extends Controller
     public function create(): View
     {
         $user = Auth::user();
-        if (!empty($user)) {
+        if (!empty($user) && $user->role === "admin") {
             return view("admin.offer", compact("user"));
         } else {
             return abort(404);
         }
     }
 
-    public function store(Request $request): array
+    public function store(Request $request): RedirectResponse|array
     {
-        $this->validateOffer($request);
-        $offer = new Offer();
-        $offer->fill($request->except('photos'));
-        $file_paths = [];
-        if ($request->hasFile('photos')) {
-            foreach ($request->file('photos') as $photo) {
-                $path = $photo->storeAs('photos',  $photo->getClientOriginalName(), 'uploads');
-                $file_paths[] = $path;
+        if (Auth::check() && Auth::user()->role === 'admin') {
+            $this->validateOffer($request);
+            $offer = new Offer();
+            $offer->fill($request->except('photos'));
+            $file_paths = [];
+            if ($request->hasFile('photos')) {
+                foreach ($request->file('photos') as $photo) {
+                    $path = $photo->storeAs('photos',  $photo->getClientOriginalName(), 'uploads');
+                    $file_paths[] = $path;
+                }
+                $offer->photo_path = implode(',', $file_paths);
+                $offer->save();
+                return redirect()->back()->with('message', 'New property offer has been added');
+            } else {
+                $offer->save();
+                return redirect()->back()->with('message', 'New property offer has been added. 
+            Please consider to add photos a better experience.');
             }
-            $offer->photo_path = implode(',', $file_paths);
-            $offer->save();
-            return ['message' => 'New property offer has been added'];
         } else {
-            $offer->save();
-            return ['message' => 'New property offer has been added. Please consider to add photos a better experience.'];
+            return ['message' => '404, not authorized'];
         }
     }
 
