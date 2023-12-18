4486e2f29c4acfb97520cfca7a63604c8ffc27be
EyK-26 <efeyagizkiziltas@gmail.com>
2023-12-18 17:06:52 +0100

    42

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -23,9 +23,20 @@ class OfferController extends Controller
     {
         $this->validateOffer($request);
         $offer = new Offer();
-        $offer->fill($request->all());
-        $offer->save();
-        return ['message' => 'New property offer has been added'];
+        $offer->fill($request->except('photos'));
+        $file_paths = [];
+        if ($request->hasFile('photos')) {
+            foreach ($request->file('photos') as $photo) {
+                $path = $photo->storeAs('photos',  $photo->getClientOriginalName(), 'uploads');
+                $file_paths[] = $path;
+            }
+            $offer->photo_path = implode(',', $file_paths);
+            $offer->save();
+            return ['message' => 'New property offer has been added'];
+        } else {
+            $offer->save();
+            return ['message' => 'New property offer has been added. Please consider to add photos a better experience.'];
+        }
     }
 
     private function validateOffer(Request $request): void
