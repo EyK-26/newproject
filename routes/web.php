601c36917c25ddac263fd10b10a6378123b04d3e
EyK-26 <efeyagizkiziltas@gmail.com>
2024-01-02 10:57:56 +0100

    123

--- routes/web.php
+++ routes/web.php
@@ -13,26 +13,19 @@ Route::group(['middleware' => 'can:admin'], function () {
     Route::get('/enquiry/{offer_id}/{user_id}', [EnquiryController::class, 'show'])
         ->whereNumber(['offer_id', 'user_id'])
         ->name('enquiry.show');
-
     Route::get('/enquiry/answer/{enquiry_id}', [EnquiryController::class, 'answer'])
         ->whereNumber('enquiry_id')
         ->name('enquiry.answer');
-
     Route::post('/answers', [AnswerController::class, 'store'])
         ->name('answers.store');
-
     Route::get('/offer', [OfferController::class, 'create'])
         ->name('offer.create');
-
     Route::post('/offer', [OfferController::class, 'store'])
         ->name('offer.store');
-
     Route::get('/offers', [OfferController::class, 'index'])
         ->name('offers.index');
-
     Route::get('/offers/{offer}', [OfferController::class, 'edit'])->whereNumber('offer')
         ->name('offers.edit');
-
     Route::post('/offers/{offer}', [OfferController::class, 'update'])->whereNumber('offer')
         ->name('offers.update');
 });
