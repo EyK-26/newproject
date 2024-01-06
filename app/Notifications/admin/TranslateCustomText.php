c7a7f0dd1fdfa6ae88f37a5bba7c039c7b0810a2
EyK-26 <efeyagizkiziltas@gmail.com>
2024-01-06 15:47:18 +0100

    #43

--- app/Notifications/admin/TranslateCustomText.php
+++ app/Notifications/admin/TranslateCustomText.php
@@ -2,16 +2,25 @@
 
 namespace App\Notifications\admin;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Support\Facades\Auth;
 
 class TranslateCustomText
 {
     public static function translate(string $source): string
     {
-        $authKey =
-            "393c4bea-d902-58be-5818-ad4f90aa90e4:fx";
-        $target_language = Auth::user()->language;
-        $translator = new \DeepL\Translator($authKey);
-        return $translator->translateText($source, null, $target_language); //when null it's auto detected.
+        if (!empty(Auth::user())) {
+            $target_language = Auth::user()->language;
+            if (isset($target_language)) {
+                $authKey =
+                    "393c4bea-d902-58be-5818-ad4f90aa90e4:fx";
+                $translator = new \DeepL\Translator($authKey);
+                return $translator->translateText($source, null, $target_language); //when null it's auto detected.
+            } else {
+                return $source;
+            }
+        } else {
+            return $source;
+        }
     }
 }
